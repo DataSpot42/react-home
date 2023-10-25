803e19823ddd10a9812868ec1a12a924e33db0d2
DataSpot42 <stevelookingforward@gmail.com>
2023-10-25 11:12:46 +0100

    updates to some discriptions
    Signed-off-by: DataSpot42 <stevelookingforward@gmail.com>

--- src/components/ecommappCards.js
+++ src/components/ecommappCards.js
@@ -1,28 +1,35 @@
 const ECommAppCards = () => {
-    const eComCard = [{title: "PunkIPA",
-                subTitle: "Full Stack GitHub Repo",
-                text: "GitHub Repository for the full stack project to create am ecommerse app using an API for the content and a MondoDB for the backend database.",
-                image: "./images/Github_logo.png",
-                imageAlt: "PunkIPA GitHub Repo",
-                linkUrl: "https://github.com/stefanthomasjones/PunkSite",
-                linkText: "PunkSite Repo" },
-            {title: "PunkIPA",
+    const eComCard = [
+        {
+            title: "PunkIPA",
             subTitle: "Front-End App",
-            text: "App, using browser local storage to demonstraite the app.",
+            text: "Part of the Bootcamp, the Punk IPA app was developed by myself and fellow student Stefan.  Using an API to get the product information, a price generator NPM module we put together the PunkIPA App.  This is a front end only version, using local storage.",
             image: "./images/punkIPAApp.png",
             imageAlt: "PunkIPA App",
             linkUrl: "https://punkipa.netlify.app/",
-            linkText: "PunkIPA App" },
-            {title: "PunkIPA",
+            linkText: "PunkIPA App"
+        },
+        {
+            title: "PunkIPA",
+            subTitle: "Full Stack GitHub Repo",
+            text: "GitHub Repository for the full stack version, using MondoDB for the backend database.",
+            image: "./images/Github_logo.png",
+            imageAlt: "PunkIPA GitHub Repo",
+            linkUrl: "https://github.com/stefanthomasjones/PunkSite",
+            linkText: "PunkSite Repo"
+        },
+        {
+            title: "PunkIPA",
             subTitle: "Front-End Repo",
             text: "GitHub Repository for the front-end demo",
             image: "./images/Github_logo.png",
             imageAlt: "PunkIPA FE GitHub Repo",
             linkUrl: "https://github.com/DataSpot42/punk-local-app",
-            linkText: "PunkSite FE Repo" }
-            
-                    
-        ]
-return (eComCard)
+            linkText: "PunkSite FE Repo"
+        }
+
+
+    ]
+    return (eComCard)
 }
 export default ECommAppCards
