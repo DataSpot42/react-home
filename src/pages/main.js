f82aa211caae96951837492d7150d76aa87acca2
DataSpot42 <stevelookingforward@gmail.com>
2023-10-25 11:56:40 +0100

    styling alrerations
    Signed-off-by: DataSpot42 <stevelookingforward@gmail.com>

--- src/pages/main.js
+++ src/pages/main.js
@@ -15,22 +15,22 @@ const MainPage = () => {
   <CCarouselItem className="caraItem">  
   <iframe className="caraApp" title="WeatherApp" src="https://weatherappbc2023.netlify.app/" allow="geolocation" width="100%" height="99.4%" ></iframe>
   <div className="caraText">Weather App written during bootcamp and updated since to add new features. <p>Click on link below to open the app</p></div>
-  <div className="button-container"><a className = "bigBut" href="https://weatherappbc2023.netlify.app/" target="_blank" rel="noreferrer">Weather App (new tab)</a></div>
+  <div className="button-container"><a className = "cardButton" href="https://weatherappbc2023.netlify.app/" target="_blank" rel="noreferrer">Weather App</a></div>
   </CCarouselItem>
   <CCarouselItem className="caraItem">
   <iframe className="caraApp" title="To Do App" src="https://todotodos.netlify.app/" width="100%" height="99.4%" ></iframe>
   <div className="caraText">To Do app written during bootcamp. <p>Click on link below to open app.</p></div>
-  <div className="button-container"><a className = "bigBut" href="https://todotodos.netlify.app/" target="_blank" rel="noreferrer">To do App (new tab)</a></div>
+  <div className="button-container"><a className = "cardButton" href="https://todotodos.netlify.app/" target="_blank" rel="noreferrer">To do App</a></div>
   </CCarouselItem>
   <CCarouselItem className="caraItem">
   <iframe className="caraApp" title="Card Games App" src="https://play-card-games.netlify.app/" width="100%" height="99.4%" ></iframe>
   <div className="caraText">Card Game App derived from Card Dealer script written at the start of the bootcamp. Click on link below to open the app</div>
-  <div className="button-container"><a className = "bigBut" href="https://play-card-games.netlify.app" target="_blank" rel="noreferrer">Card Games (new tab)</a></div>
+  <div className="button-container"><a className = "cardButton" href="https://play-card-games.netlify.app" target="_blank" rel="noreferrer">Card Games</a></div>
   </CCarouselItem>
   <CCarouselItem className="caraItem">
   <iframe className="caraApp" title="PunkIPA" src="https://punkipa.netlify.app/" width="100%" height="99.4%" ></iframe>
   <div className="caraText">eCommerce App complted during bootcamp with a fellow student. Click on link below to open the app</div>
-  <div className="button-container"><a className = "bigBut" href="https://punkipa.netlify.app/" target="_blank" rel="noreferrer">PunkIPA App (new tab)</a></div>
+  <div className="button-container"><a className = "cardButton" href="https://punkipa.netlify.app/" target="_blank" rel="noreferrer">PunkIPA App</a></div>
   </CCarouselItem>
 </CCarousel>
 </div>
