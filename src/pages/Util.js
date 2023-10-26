497700545a5ed05fee9a8a19d370840a6389c4a1
DataSpot42 <stevelookingforward@gmail.com>
2023-10-26 15:21:16 +0100

    Link issues sorted
    Signed-off-by: DataSpot42 <stevelookingforward@gmail.com>

--- src/pages/Util.js
+++ src/pages/Util.js
@@ -1,6 +1,6 @@
 import '../App.css';
 import './appPages.css'
-
+import { Link } from 'react-router-dom';
 import Card from '../components/Card';
 
 import UtilCards from '../components/utilCards';
@@ -15,12 +15,26 @@ const Util = () => {
          <div className="appGrid-container">
          {appCards ? appCards.map((appCards, index) => <div className="appCards">
          <Card appCard={appCards}/>
+         
 
-</div>) : <p>Loading...</p>}
+</div>
+) : <p>Loading...</p>}
+<div className="appCards">
+<div className="card-container">
+            <div className="appTitle">TechDog.blog</div>
+            <div className="appSubTitle">Blogging Platform Slideshow</div>
+            <div className="appText">Here is a slideshow of the development of our final Bootcamp assignment.</div>
+            <div className="appImage"><img src="./images/Slide2.svg" alt="TechDog.blog SlideShow"></img></div>            
+            <div id="bottom">
+            <Link className = "cardButton" to="/TechDogBlogPPT">TechDogBlogPPT</Link>
+            </div>
+        </div>
+        </div>
             </div>   
             </div>   
         
 
     )
     }
-    export default Util;       
+    export default Util;   
+    
