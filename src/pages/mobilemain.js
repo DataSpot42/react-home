737e0968106bd8e235c937347c8eff8903200c9a
DataSpot42 <stevelookingforward@gmail.com>
2023-12-04 16:41:03 +0000

    Added welcome bit
    Signed-off-by: DataSpot42 <stevelookingforward@gmail.com>

--- src/pages/mobilemain.js
+++ src/pages/mobilemain.js
@@ -5,7 +5,11 @@ import '../App.css';
 const MobileMain = () => {
 
     return (
+        
         <div className="appPages">
+            <div className="eachApp"> 
+            <div className="scrollText">
+                Welcome to the camelCode React Portfolio App.  I'm Steve, a Software Developer based in the North West of England.  Having completed a Bootcamp earlier in the year this app allows be to demonstraite my coding skills.  Please take a look around, try out the apps or check out my code in GitHub.  </div></div>
             <div className="eachApp">
                 <iframe className="scrollApp" title="WeatherApp" src="https://weatherappbc2023.netlify.app/" allow="geolocation" width="100%" height="99.4%" ></iframe>
                 <div className="scrollText">Weather App written during bootcamp and updated since to add new features. <p>Click on button below to open the app or look at the GitHub Repo for the code</p></div>
